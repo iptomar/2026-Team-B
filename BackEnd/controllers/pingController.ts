36e99fc8c37c6eb4dcc3cbc82ddbb1f0e3248b0d
Paulo Pereira <tisppereira@gmail.com>
2026-04-12 01:53:46 +0100

    feat: integrate TSOA for automated API routing and Swagger documentation generation. test endpoint

---
+++ BackEnd/controllers/pingController.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Route } from "tsoa";
+
+@Route("ping")
+export class PingController extends Controller {
+  @Get("/")
+  public async getMessage(): Promise<{ message: string }> {
+    return {
+      message: "pong"
+    };
+  }
+}

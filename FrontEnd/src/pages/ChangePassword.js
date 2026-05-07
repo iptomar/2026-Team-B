7d635ede3d4467218fa3eb180f3f5204a8d3dc96
Joana Flores <jriverunderscore@gmail.com>
2026-05-07 19:53:53 +0100

    Update ChangePassword.js

--- FrontEnd/src/pages/ChangePassword.js
+++ FrontEnd/src/pages/ChangePassword.js
@@ -27,7 +27,7 @@ const ChangePassword = () => {
 
 		try {
 			const accessToken = localStorage.getItem('accessToken');
-			const res = await fetch(`http://localhost:5000/auth/reset-password`, {
+			const res = await fetch(`${apiUrl}/auth/reset-password`, {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
